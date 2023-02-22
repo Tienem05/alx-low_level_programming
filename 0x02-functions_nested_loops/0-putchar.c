f6da26f813d9f734be8665a3d8876704f29a412e
Tienem05 <nashthuo5@gmail.com>
2023-02-21 23:49:01 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
-#include "stdio.h"
+#include"main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ *main-mainfunction
+ *_putchar: print the alphabets putchar
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-	char *sh = "_putchar";
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);	
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,31 +1,6 @@
-void times_table(void)
-{
-	int row, col;
-	int mult = 0;
+#ifndef MAIN_H
+#define MAIN_H
 
-	for (row = 0; row < 10; row++)
-	{
-		for (col = 0; col < 10; col++)
-		{
-			mult = (row * col);
-			if (mult < 10)
-			{
-				if (col != 0)
-				{
-					__putchar_(' ');
-					__putchar_(' ');
-				}
-				__putchar_(mult + '0');
-			}
-			else
-			{
-				__putchar_(' ');
-				__putchar_((mult / 10) + '0');
-				__putchar_((mult % 10) + '0');
-			}
-			if (col != 9)
-				__putchar_(',');
-		}
-		__putchar_('\n');
-	}
-}
+int _putchar(char c);
+
+#endif
