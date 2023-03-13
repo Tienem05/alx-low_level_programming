ee8e96fdf370ea4b8fd27044cdb400cd153775f8
Tienem05 <nashthuo5@gmail.com>
2023-03-13 10:51:33 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,32 +13,34 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **twod;
-	int a, b, j, k;
+	int **twoD;
+	int hgt_index, wid_index;
 
-	a = b = j = 0;
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	/* using typecasting (int *) to dereference values */
-	twod = malloc(height * sizeof(int *));
-	if (twod == NULL)
-		return (NULL);
-	for (a = 0; a < height; a++)
-	{
-		twod[a] = malloc(width * sizeof(int));
 
-		if (twod[a] == NULL)
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
+		return (NULL);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
 		{
-			for (k = 0; k < a; k++)
-				free(twod[k];
-			free(twod);
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+			free(twoD);
 			return (NULL);
 		}
 	}
-	for (b = 0; b < height; b++)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		for (j = 0; j < width; j++)
-			twod[b][j] = 0;
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
 	}
-	return (twod);
+	return (twoD);
 }
+
